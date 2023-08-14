07789f8bdce9c3fbd238348f592c1d1ededdc157
Trong Nghia <trongnghia77@live.com>
2023-08-14 10:32:10 +0700

    6-pygame-gameloop

--- part2-pygame/mygame.py
+++ part2-pygame/mygame.py
@@ -13,12 +13,20 @@ background_color = (255, 255, 255)
 screen.fill(background_color)
 pygame.display.update()
 
+# Thiết lập thời gian
+clock = pygame.time.Clock()
+
 # Vòng lặp chính của ứng dụng
 running = True
 while running:
+    clock.tick(60)  # Giới hạn 60 lần lặp mỗi giây
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+    # Cập nhật trạng thái của trò chơi (nhân vật, điểm số, các sự kiện, các hình ảnh, ...)
+    pygame.display.update()
 
 # Kết thúc Pygame
 pygame.quit()
