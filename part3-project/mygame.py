e95594ffc8d20a0934b8c2a5cef28c74454c04b5
Trong Nghia <trongnghia77@live.com>
2023-08-15 15:51:03 +0700

    11-project-player

--- part3-project/mygame.py
+++ part3-project/mygame.py
@@ -29,6 +29,25 @@ class GameObject:
     def draw(self, background):
         background.blit(self.image, (self.x_pos, self.y_pos))
 
+# Class cho nhân vật game
+class PlayerCharacter(GameObject):
+    # Tốc độ di chuyển
+    SPEED = 10
+
+    def __init__(self, image_path, x, y, width, height):
+        super().__init__(image_path, x, y, width, height)
+
+    # Hàm cho phép nhân vật di chuyển, direction > 0 => đi lên, ngược lại đi xuống
+    def move(self, direction, max_height):
+        if direction > 0:
+            self.y_pos -= self.SPEED
+        elif direction < 0:
+            self.y_pos += self.SPEED
+        # Đảm bảo nhân vật không di chuyển vượt khỏi màn hình bên dưới
+        if self.y_pos >= max_height - 40:
+            self.y_pos = max_height - 40
+
+
 class Game:
     TICK_RATE = 60 # FPS
 
@@ -49,9 +68,13 @@ class Game:
 
     def run_game_loop(self):
         is_game_over = False
+        direction = 0
 
         # Khởi tạo đối tượng hộp kho báu
         treasure = GameObject('treasure.png', 375, 50, 50, 50)
+
+        # Khởi tạo nhân vật
+        player_character = PlayerCharacter('player.png', 375, 700, 50, 50)
 
         # Gameloop
         while not is_game_over:
@@ -59,6 +82,19 @@ class Game:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     is_game_over = True
+                # Sự kiện xảy ra khi có nút được nhấn xuống
+                elif event.type == pygame.KEYDOWN:
+                    # Nút nhấn là mũi tên lên
+                    if event.key == pygame.K_UP:
+                        direction = 1
+                    # Nút nhấn là mũi tên xuống
+                    elif event.key == pygame.K_DOWN:
+                        direction = -1
+                # Sự kiện xảy ra khi không còn nhấn nút
+                elif event.type == pygame.KEYUP:
+                    if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                        direction = 0
+                # print(event)
 
             # Vẽ lại màn hình
             self.game_screen.fill(WHITE_COLOR)
@@ -66,6 +102,11 @@ class Game:
 
             # Vẽ hộp kho báu
             treasure.draw(self.game_screen)
+
+            # Cập nhật vị trí của nhân vật sau di chuyển
+            player_character.move(direction, self.height)
+            # Vẽ nhân vật ra màn hình
+            player_character.draw(self.game_screen)
 
             # Update
             pygame.display.update()
